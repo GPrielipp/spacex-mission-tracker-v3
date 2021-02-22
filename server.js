b745b3e6d2a20ad9c143b1bd957c03160006bc4b
Hidden-Bug-0x1 <gwprielipp@gmail.com>
2021-02-21 20:24:10 -0900

    heroku build

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const axios = require('axios');
 const { graphqlHTTP } = require('express-graphql');
+const cors = require('cors');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 const schema = require('./schema');
+
+app.use(cors());
 
 app.use(
   '/graphql',
@@ -14,5 +17,11 @@ app.use(
     graphiql: true,
   })
 );
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
